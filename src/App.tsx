7c26030096f8b6c78e1d9f27767db667877ffe90
Sergei Ivanov <38918636+sergeiivanow@users.noreply.github.com>
2023-03-13 23:30:31 +0300

    Add some info

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <div>{'Hello.'}</div>
+       <div className="App-name">{`I'm Sergei`}</div>
       </header>
+      <div className="App-description">{`Professional programmer in cross-platform mobile development with more than 4 years' experience`}</div>
     </div>
   );
 }
