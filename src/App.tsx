c537dacac07a0d0ac938b6bc2a357d68d3b725a7
Sergei Ivanov <38918636+sergeiivanow@users.noreply.github.com>
2023-03-15 12:23:03 +0300

    Add the info

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,10 @@ function App() {
       </header>
       <div className="App-description">{`Professional programmer in cross-platform mobile development with more than 4 years' experience`}</div>
       <img className="App-photo" src={photo} alt="Photo" />
+      <a href="https://www.dropbox.com/s/x786wagua9eb0ue/s.ivanov_rn_dev_resume_ru.pdf?dl=1" className="App-link">{'Get my resume (RU)'}</a>
+      <a href="mailto:to@sivanow.ru" className="App-link">{'Get in touch'}</a>
+      <a href="https://t.me/sergeiiwanov" target="_blank" className="App-link" rel="noreferrer">{'Telegram'}</a>
+      <a href="https://github.com/sergeiivanow" target="_blank" className="App-link" rel="noreferrer">{'GitHub'}</a>
     </div>
   );
 }
